9aa61bac59d678a357692a27dac1074037163410
Allawi <aliallawi069@gmail.com>
2025-11-06 20:04:17 +0300

    Add temperature series and filter hot cities

--- Series/Series.py
+++ Series/Series.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+temperatures = pd.Series(
+    [25, 32, 29, 35, 40],
+    index=["Baghdad", "Basra", "Mosul", "Erbil", "Karbala"]
+)
+
+hot_cities = temperatures[temperatures > 30]
+print(hot_cities)cities)[temperatures > 30]
+print(hot_cities)
