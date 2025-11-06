7ec73ebfcc60ea428c3167a1f5b032fdf13d81ae
Allawi <aliallawi069@gmail.com>
2025-11-06 20:06:18 +0300

    Add product DataFrame and CSV export functionality
    Create a DataFrame for products and save to CSV

--- Series/DataFrame/DataFrame.py
+++ Series/DataFrame/DataFrame.py
@@ -1,1 +1,15 @@
+import pandas as pd
 
+products = pd.DataFrame({
+    "name": ["Laptop", "Phone", "Tablet"],
+    "price": [1200, 800, 400],
+    "quantity": [5, 10, 8]
+})
+
+products["total_value"] = products["price"] * products["quantity"]
+products = products.sort_values("total_value", ascending=False)
+
+products.to_csv("products.csv", index=False)
+products_loaded = pd.read_csv("products.csv")
+
+print(products_loaded)
